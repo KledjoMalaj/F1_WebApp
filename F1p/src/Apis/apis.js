56b1973ff8eb60dbf8e96d06c8ac4edd48d43d95
Kledjo Malaj <kledjomalaj20@gmail.com>
2026-02-21 22:17:18 +0100

    adding drivers standings in drivers page

--- F1p/src/Apis/apis.js
+++ F1p/src/Apis/apis.js
@@ -1,3 +1,29 @@
-export function Test(){
-  console.log("testing")
+const year = "2025"
+
+const baseUrl = "https://api.jolpi.ca/ergast/f1"
+
+export async function fetchDrivers(){
+  try {
+  const res = await fetch(`${baseUrl}/${year}/driverstandings/`)
+  const data = await res.json()
+
+  let driversArr = []
+
+  data.MRData.StandingsTable.StandingsLists[0].DriverStandings.map(driver => {
+      let drivers = {
+        driverId:driver.Driver.driverId,
+        name:driver.Driver.givenName,
+        lastName:driver.Driver.familyName,
+        constructor:driver.Constructors[0].name,
+        wins:driver.wins,
+        position:driver.position,
+        points:driver.points,
+      }
+      driversArr.push(drivers)
+    })
+
+    return driversArr
+  } catch (err) {
+    return []
+  }
 }
--- F1p/src/pages/Driverpage.jsx
+++ F1p/src/pages/Driverpage.jsx
@@ -1,7 +1,32 @@
+import {fetchDrivers} from "../Apis/apis.js" 
+import React, {useEffect, useState} from "react";
+
 function DriverPage() {
+  const [drivers, setDrivers] = useState([])
+
+  useEffect(() => {
+    async function getData(){
+      const drivers = await fetchDrivers()
+      setDrivers(drivers)
+    }
+    getData()
+  }, [])
+
   return(
     <>
       <h1>Drivers Page</h1>
+      <div>
+        {drivers.map(driver => (
+        <div key={driver.position} className='bg-gray-400 m-3 p-3 flex justify-between'>
+            <h1 key={driver.position}>{driver.position}</h1>
+            <h1 key={driver.position}>{driver.name}</h1>
+            <h1 key={driver.position}>{driver.lastName}</h1>
+            <h1 key={driver.position}>{driver.points}</h1>
+            <h1 key={driver.position}>{driver.wins}</h1>
+            <h1 key={driver.postition}>{driver.constructor}</h1>
+        </div>
+        ))}
+      </div>
     </>
   )
 }
